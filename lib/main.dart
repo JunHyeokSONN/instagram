0ca45b4cf53ccae0f9e60e6330773604351dcfc0
JunHyeokSONN <https://github.com/JunHyeokSONN>
2023-09-22 14:06:34 +0900

    ff

--- lib/main.dart
+++ lib/main.dart
@@ -18,14 +18,10 @@ import './notification.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-
 void main() async {
 
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-
-  );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
   runApp(
       MultiProvider(
         providers: [
